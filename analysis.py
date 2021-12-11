e75ea15dfb06c434c103633c721e5f314c7936d2
MikeDoes <40600201+MikeDoes@users.noreply.github.com>
2021-12-11 15:02:13 +0100

    Added new design

--- analysis.py
+++ analysis.py
@@ -15,16 +15,41 @@ for entry in data:
 print('Length of entries', len(data))
 print('Length of paragraphs', len(unique_paragraph_list))
 
+category_count = {}
+for element in data:
+    category = element['category']
+
+    if category not in category_count.keys():
+        category_count[category] = 0
+
+    category_count[category] += 1
+    
+print('category count', category_count)
+
+    
+    
+
+
 ### Creating a new dataset with the same parahprah as list
 new_json = []
 for paragraph in unique_paragraph_list:
     entities = []
-    for i, entry in enumerate(data):
+    for i, entry in reversed(list(enumerate(data))):
         if paragraph == entry['paragraph']:
             del entry['paragraph']
             entities += [entry]
             del data[i]
+        entities = entities[::-1]
     new_json += [{'paragraph': paragraph, 'entities':entities}]
+
+
+
+
+#Check that all entities were transferred:
+entity_count = 0
+for item in new_json:
+    entity_count += len(item['entities'])
+
 
 
 with open('fin_num_merged.json','w') as f:
